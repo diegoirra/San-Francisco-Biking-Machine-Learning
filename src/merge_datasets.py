78dd481190a1c9ab5a134c415bd018a683c63cde
Diego Irra <dgirra@hotmail.com>
2017-06-13 22:02:40 -0300

    Me falto el merge del filtered

--- src/merge_datasets.py
+++ src/merge_datasets.py
@@ -18,13 +18,17 @@ os.chdir("..")
 weather = pd.read_csv('data/parsed_weather.csv', low_memory=False)
 station = pd.read_csv('data/parsed_station.csv', low_memory=False)
 train = pd.read_csv('data/parsed_trip_train.csv', low_memory=False)
+train_filtered = pd.read_csv('data/parsed_trip_train_filtered.csv', low_memory=False)
 test = pd.read_csv('data/parsed_trip_test.csv', low_memory=False)
 
+
+train_with_stations_filtered = pd.merge(train,station, how='inner', left_on='start_station_id', right_on='station_id')
+train_with_stationweather_filtered = pd.merge(train_with_stations_filtered, weather, on=['date','city'], how='inner')
+train_with_stationweather_filtered.to_csv('data/trips_train_with_stationweather_filtered.csv', index=False)
 
 train_with_stations = pd.merge(train,station, how='inner', left_on='start_station_id', right_on='station_id')
 train_with_stationweather = pd.merge(train_with_stations, weather, on=['date','city'], how='inner')
 train_with_stationweather.to_csv('data/trips_train_with_stationweather.csv', index=False)
-
 
 test_with_stations = pd.merge(test,station, how='inner', left_on='start_station_id', right_on='station_id')
 test_with_stationweather = pd.merge(test_with_stations, weather, on=['date','city'], how='inner')
